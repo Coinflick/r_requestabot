48b118be391b5298e33a338e0b94e563fae071f9
Harry <cuzimapenguin@gmail.com>
2017-10-27 17:50:54 -0400

    Update main_cog.py

--- discordapp - send_code/main_cog.py
+++ discordapp - send_code/main_cog.py
@@ -8,7 +8,7 @@ class main_cog():
 
     @commands.command(pass_context=True)
     async def code(self, ctx):
-        self.bot.send_message(ctx.message.author, str(random.randint(1000, 9000)))
+        await self.bot.send_message(ctx.message.author, str(random.randint(1000, 9000)))
         
 def setup(bot):
     bot.add_cog(main_cog(bot))
